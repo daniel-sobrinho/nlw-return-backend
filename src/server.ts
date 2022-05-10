b4b43014585300c7c87da6ccd37eadef45ae7682
Daniel Sobrinho <danielsobrinho.dev@gmail.com>
2022-05-10 10:46:55 -0300

    change message on server running

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(process.env.PORT || 3333, () => {
-    console.log(`server running | http://localhost/${process.env.PORT}`)
+app.listen(process.env.PORT, () => {
+    console.log(`server running`)
 })
